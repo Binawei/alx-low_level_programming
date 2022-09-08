0b2d42efda310ab105a5011b7e3e7b4a50e50c5a
Binawei <gbamilabina@gmail.com>
2022-09-08 14:13:07 -0700

    printf usage

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * printf
- * /
- **/
+ *main-Entry point
+ *Return: Always 0 (success)
+*/
 int main(void)
 {       printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
 }
