23927ddaae06edb68e7cc2a3b9e0236dfb31a6be
Binawei <gbamilabina@gmail.com>
2022-09-10 01:05:54 -0700

    takes input and gives output

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <time.h>
 /**
  * main-Entry point
  * Return: Always 0 (success)
@@ -8,8 +7,6 @@
  */
 int main(void)
 {      int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 	scanf("%d", &n);
 	if (n > 0)
 	{
