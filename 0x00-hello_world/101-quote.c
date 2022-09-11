168a4cc674e2188ed316c5747bcb02ae50a9c2e8
Binawei <gbamilabina@gmail.com>
2022-09-11 11:45:27 -0700

    quotes

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <fcntl.h>
+/**
+ * main - and that piece of art is useful
+ *
+ * Return: Always (0) success
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
