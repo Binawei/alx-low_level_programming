9a1f0b46353ba2e6437554a93c59e0a90138cf7d
Binawei <gbamilabina@gmail.com>
2022-09-14 03:10:16 -0700

    lowercase letters checks

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
- * main - function that checks for lowercase character.
- * _islower(): lower case letters
+ *_islower - function that checks for lowercase character.
  *
- * 'c' is the character for the alphbet
+ * @c: is the character for the alphbet
  * Return: Always 0
  */
 int _islower(int c)
 {
 
-	c = 'a';
-	if (c >= 'a' && c <= 'z')
+	if (c == 'a' && c <= 'z')
 	{
 		return (1);
 	}
