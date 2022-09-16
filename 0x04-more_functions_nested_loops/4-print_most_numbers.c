e750f38c1c0709a24f2deb62c4c0127489d8f65a
Binawei <gbamilabina@gmail.com>
2022-09-16 05:48:04 -0700

    prints numbers except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,13 @@ void print_most_numbers(void)
 {
 	int i;
 
-	i = 0;
-	while (i <= 9)
+	for (i = 0; i < 10; i++)
 	{
 		if ((i == 2) || (i == 4))
 		{
 			continue;
 		}
-		_putchar(i + '0');
-		i++;
+		_putchar(i);
 	}
 	_putchar('\n');
 }
