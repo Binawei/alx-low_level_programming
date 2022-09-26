50faa8830a70e3d8f61bd8f516c4851968fdea5a
Binawei <gbamilabina@gmail.com>
2022-09-26 05:53:22 -0700

    a function that copies memory area.

---
+++ 0x07-pointers_arrays_strings/1-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - calls other functions
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *_memcpy(char *dest, char *src, unsigned int n);
+	return (0);
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,2 +1,3 @@
 int _putchar(char c);
 function that fills memory with a constant byte.
+a function that copies memory area.
