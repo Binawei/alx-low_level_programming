751494ad094c53653e4b54413dcc9754bc7f72b9
Binawei <gbamilabina@gmail.com>
2022-09-28 06:50:58 -0700

    function that returns the length of a string.

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - calls other functions
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int _strlen_recursion(char *s);
+	return (0);
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,4 @@
 print a character
 print a character using recursion
+function that returns the length of a string.
+
