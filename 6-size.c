7bac911ef6409c45cbc9a4513c9af1892b28ea97
Binawei <gbamilabina@gmail.com>
2022-09-30 10:40:20 -0700

    a program that prints its name, followed by a new line

---
+++ 6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - a C program that prints the size of various types on the computer
+ * 
+ * Return: Always (0) succsess
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long longType;
+	float floatType;
