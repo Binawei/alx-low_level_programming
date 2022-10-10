21723f73d6d1a0abe105ecd98a0f8ba71d9305b0
Binawei <gbamilabina@gmail.com>
2022-10-10 10:47:38 -0700

    a struct for dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a structure for dogs
+ * @name: member 1.
+ * @age: member 2.
+ * @owner: third member
+ *
+ * Discription: define a new type struct dog with the element
+ * name, age amd owner.
+ */
 struct dog
 {
 	char *name;
