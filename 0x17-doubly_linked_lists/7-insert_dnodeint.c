36948c29621b41cf6c48f423afe6337a993e7b6c
Binawei <gbamilabina@gmail.com>
2022-12-09 05:30:15 -0800

    inserts a new node at a given position.

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 
 	count = 0;
-	while (count < (idx - 1))
+	while (count <= (idx - 1))
 	{
 		temp = temp->next;
 		count++;
